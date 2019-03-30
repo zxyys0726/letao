64bb23c1319f2ff03de8f6b3516722d15ef5ef53
zxyys0726 <825322539@qq.com>
2019-03-30 11:34:03 +0800

    公共部分功能

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -5,5 +5,19 @@ $(document).ajaxStart(function () {
 $(document).ajaxStop(function () {
   setTimeout(function () {
     NProgress.done()
-  }, 2000)
-})
+  }, 500)
+});
+
+if (location.href.indexOf('login.html') === -1) {
+  $.ajax({
+    type: "get",
+    url: "/employee/checkRootLogin",
+    dataType: "json",
+    success: function (info) {
+      console.log(info);
+      if (info.error === 400) {
+        location.href = 'login.html';
+      }
+    }
+  });
+}
