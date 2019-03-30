999d22faf7bb8e131ceb8cc238ca2baea1c4a309
zxyys0726 <825322539@qq.com>
2019-03-30 21:46:11 +0800

    分页模块

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -21,3 +21,35 @@ if (location.href.indexOf('login.html') === -1) {
     }
   });
 }
+
+$(function(){
+  $('.category').on('click',function(){
+    $('.child').stop().slideToggle();
+  })
+
+  $('.icon-menu').on('click',function(e){
+    e.preventDefault();
+    $('.lt-aside').toggleClass("hidemenu");
+    $('.lt-main').toggleClass("hidemenu");
+    $('.topbar').toggleClass("hidemenu");
+    $('.container-fluid').toggleClass("hidemenu");
+  })
+
+  $('.icon-logout').click(function(){
+    $('#myModal').modal('show')
+  })
+
+  $('.btn-logout').click(function(){
+    $.ajax({
+      type: "get",
+      url: "/employee/employeeLogout",
+      dataType: "json",
+      success: function (info) {
+        console.log(info);
+        if (info.success) {
+          location.href = 'login.html'
+        }
+      }
+    });
+  })
+})
---
+++ public/back/js/user.js
@@ -0,0 +1,35 @@
+$(function(){
+  var currentPage = 1;
+  var pageSize = 5;
+  render();
+  function render(){
+    $.ajax({
+      type: "get",
+      url: "/user/queryUser",
+      data: {
+        page:currentPage,
+        pageSize:pageSize
+      },
+      dataType: "json",
+      success: function (info) {
+        console.log(info);
+        var htmlStr = template('tpl',info);
+        $('tbody').html(htmlStr);
+  
+        $("#pagintor").bootstrapPaginator({
+          bootstrapMajorVersion:3,//默认是2，如果是bootstrap3版本，这个参数必填
+          currentPage:info.page,//当前页
+          totalPages:Math.ceil(info.total/info.size),//总页数
+          size:"small",//设置控件的大小，mini, small, normal,large
+          onPageClicked:function(event, originalEvent, type,page){
+            //为按钮绑定点击事件 page:当前点击的按钮值
+            currentPage = page;
+            render();
+          }
+        });
+      }
+    });
+  }
+  
+  
+})
