c092c1ead6ff278eb5311976719b25253c6a1a77
zxyys0726 <825322539@qq.com>
2019-03-30 22:51:01 +0800

    用户管理完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,27 +1,28 @@
-$(function(){
+$(function () {
   var currentPage = 1;
   var pageSize = 5;
   render();
-  function render(){
+
+  function render() {
     $.ajax({
       type: "get",
       url: "/user/queryUser",
       data: {
-        page:currentPage,
-        pageSize:pageSize
+        page: currentPage,
+        pageSize: pageSize
       },
       dataType: "json",
       success: function (info) {
         console.log(info);
-        var htmlStr = template('tpl',info);
+        var htmlStr = template('tpl', info);
         $('tbody').html(htmlStr);
-  
+
         $("#pagintor").bootstrapPaginator({
-          bootstrapMajorVersion:3,//默认是2，如果是bootstrap3版本，这个参数必填
-          currentPage:info.page,//当前页
-          totalPages:Math.ceil(info.total/info.size),//总页数
-          size:"small",//设置控件的大小，mini, small, normal,large
-          onPageClicked:function(event, originalEvent, type,page){
+          bootstrapMajorVersion: 3, //默认是2，如果是bootstrap3版本，这个参数必填
+          currentPage: info.page, //当前页
+          totalPages: Math.ceil(info.total / info.size), //总页数
+          size: "small", //设置控件的大小，mini, small, normal,large
+          onPageClicked: function (event, originalEvent, type, page) {
             //为按钮绑定点击事件 page:当前点击的按钮值
             currentPage = page;
             render();
@@ -30,6 +31,32 @@ $(function(){
       }
     });
   }
-  
-  
+
+  var currentId
+  var isDelete
+  $('tbody').on('click', '.btn', function () {
+    $('#user-model').modal('show');
+    currentId = $(this).parent().data('id');
+    console.log(currentId);
+    isDelete = $(this).hasClass('btn-danger') ? 0 : 1;
+  })
+
+  $('.btn-confirm').click(function () {
+    $.ajax({
+      type: "post",
+      url: "/user/updateUser",
+      data: {
+        id: currentId,
+        isDelete: isDelete
+      },
+      dataType: "json",
+      success: function (info) {
+        if (info.success) {
+        $('#user-model').modal('hide');
+         render();
+        }       
+      }
+    });
+  })
+
 })
